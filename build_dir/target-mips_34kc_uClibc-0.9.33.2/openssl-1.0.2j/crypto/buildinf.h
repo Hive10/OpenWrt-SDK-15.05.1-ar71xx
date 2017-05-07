0002384f8fda1bda78d758ffec5a98e7b6e5da36
Tibor Dudlák <tibor.dudlak@gmail.com>
2017-05-07 17:32:46 +0200

    Update on Makefile PR: pull/4334

--- build_dir/target-mips_34kc_uClibc-0.9.33.2/openssl-1.0.2j/crypto/buildinf.h
+++ build_dir/target-mips_34kc_uClibc-0.9.33.2/openssl-1.0.2j/crypto/buildinf.h
@@ -59,5 +59,5 @@
         '6','_','A','S','M',' ','-','D','A','E','S','_','A','S','M','\0'
     };
     #define PLATFORM "platform: linux-mips-openwrt"
-    #define DATE "built on: Sun May  7 02:32:46 2017"
+    #define DATE "built on: Sun May  7 17:24:04 2017"
 #endif
