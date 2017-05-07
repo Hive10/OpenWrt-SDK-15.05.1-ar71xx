b469929f7649291bca11a70be2269a60427277d8
Tibor Dudlák <tibor.dudlak@gmail.com>
2017-05-07 18:00:22 +0200

    After ./scripts/feeds install jose and http-parser

--- tmp/info/.files-packageinfo.mk
+++ tmp/info/.files-packageinfo.mk
@@ -1,1 +1,7 @@
 $(eval $(call PackageDir,tang-v4,tang-v4,))
+$(eval $(call PackageDir,feeds_base_ocf-crypto-headers,feeds/base/ocf-crypto-headers,))
+$(eval $(call PackageDir,feeds_base_openssl,feeds/base/openssl,))
+$(eval $(call PackageDir,feeds_base_toolchain,feeds/base/toolchain,))
+$(eval $(call PackageDir,feeds_base_zlib,feeds/base/zlib,))
+$(eval $(call PackageDir,feeds_packages_jansson,feeds/packages/jansson,))
+$(eval $(call PackageDir,feeds_packages_jose,feeds/packages/jose,))
