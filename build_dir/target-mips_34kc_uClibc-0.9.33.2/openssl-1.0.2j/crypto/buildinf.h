d567221512f91f4954dda5749cf1135e38aced89
Tibor Dudlák <tibor.dudlak@gmail.com>
2017-05-10 11:54:35 +0200

    Fixed include path...

--- build_dir/target-mips_34kc_uClibc-0.9.33.2/openssl-1.0.2j/crypto/buildinf.h
+++ build_dir/target-mips_34kc_uClibc-0.9.33.2/openssl-1.0.2j/crypto/buildinf.h
@@ -59,5 +59,5 @@
         '6','_','A','S','M',' ','-','D','A','E','S','_','A','S','M','\0'
     };
     #define PLATFORM "platform: linux-mips-openwrt"
-    #define DATE "built on: Wed May 10 00:33:52 2017"
+    #define DATE "built on: Wed May 10 01:05:17 2017"
 #endif
