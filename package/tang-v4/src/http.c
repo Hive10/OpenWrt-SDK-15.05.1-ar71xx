88e16fb656fca7fc015d373545a6f3c25621a786
Tibor Dudlák <tdudlak@redhat.com>
2017-11-26 18:05:56 +0100

    replace vdprintf*

--- package/tang-v4/src/http.c
+++ package/tang-v4/src/http.c
@@ -157,12 +157,16 @@ http_reply(const char *file, int line,
 
     fprintf(stderr, " => %d (%s:%d)\n", code, file, line);
 
-    a = dprintf(STDOUT_FILENO, "HTTP/1.1 %d %s\r\n", code, msg);
+    FILE *fdstream = fdopen(STDOUT_FILENO, "a");
+    a = fprintf(fdstream, "HTTP/1.1 %d %s\r\n", code, msg);
+    // a = dprintf(STDOUT_FILENO, "HTTP/1.1 %d %s\r\n", code, msg);
     if (a < 0)
         return a;
+    
 
     va_start(ap, fmt);
-    b = vdprintf(STDOUT_FILENO, fmt ? fmt : "\r\n", ap);
+    // b = vdprintf(STDOUT_FILENO, fmt ? fmt : "\r\n", ap);
+    b = vfprintf(fdstream, fmt ? fmt : "\r\n", ap);
     va_end(ap);
     return b < 0 ? b : a + b;
 }
